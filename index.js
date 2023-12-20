08e03af54a4237c5f8e96dac15c3c93c7bab22d2
subrata-467 <subratabera467@gmail.com>
2023-12-20 21:40:23 +0530

    initial

---
+++ index.js
@@ -0,0 +1,9 @@
+var responsive_nav = document.getElementById("responsive_nav") 
+
+function openmenu(){
+    responsive_nav.style.left = "0";
+}
+
+function closemenu(){
+    responsive_nav.style.left = "-40vw";
+}
